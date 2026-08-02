43a6750bc9b3ac1e33dda320ffe4fb6bbbc3b818
Muhamad Padli Haikal <pdlihkl@ummi.ac.id>
2026-08-02 13:33:30 +0700

    Implement basic CLI commands

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 import json
 import logging
+import sys
 from pathlib import Path
 
-def main():
+
+def show_version():
+    print("" \
+    "Version 1.0.0")
+
+def show_help():
+    print("""
+Smart Workspace Assistant
+    
+Available Commands:
+    
+help          Show help information
+version       Show application version
+report        Show task report""")
+
+def show_unknown(command):
+    print(f"Unknown Command: {command}")  
+
+def show_report():
     project_path = Path.cwd()
 
     log_folder = project_path / "logs"
@@ -14,18 +33,42 @@ def main():
         format = "%(asctime)s - %(levelname)s - %(message)s",
         )
 
-    json_file = project_path / "data" / "tasks.json"
+    json_file = project_path/ "data" / "tasks.json"
 
     try:
         with open(json_file, "r") as file:
             data = json.load(file)
 
-        logging.info("tasks.json berhasil dibaca.")
-        print(data)
+            print("---Smart Workspace Report---")
+            tasks = data["tasks"]
 
+            if len(tasks) == 0 :
+                print("Belum ada task.")
+                return
+
+            for nomor, task in enumerate(tasks, start = 1):
+                print(f"{nomor}. {task}")
+
+        logging.info("tasks.json berhasil dibaca.")
     except FileNotFoundError:
         logging.error("tasks.json tidak ditemukan.")
         print("tasks.json tidak ditemukan")
+
+def main():
+    if len(sys.argv) < 2 :
+        show_help()
+        return
+    
+    command = sys.argv[1]
+
+    if command == "help":
+        show_help()
+    elif command == "version":
+        show_version()
+    elif command == "report":
+        show_report()
+    else:
+        show_unknown(command)
 
 if __name__ == "__main__":
     main()
